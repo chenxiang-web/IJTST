4cd585672fca94bcd1354a7f5e7d2b4e1f283194
chenxiang-web <114907198+chenxiang-web@users.noreply.github.com>
2025-02-14 16:03:05 +0800

    Add files via upload

---
+++ MLE_detect_head(3DLEBlock).py
@@ -0,0 +1,63 @@
+#-------------------------------------------------------------MLE_detect_head----------------------------
+class MLE_detect_head(nn.Module):
+    dynamic = False  # force grid reconstruction
+    export = False  # export mode
+    shape = None
+    anchors = torch.empty(0)  # init
+    strides = torch.empty(0)  # init
+
+    def __init__(self, nc=80, ch=()):  # detection layer
+        super().__init__()
+        self.nc = nc  # number of classes
+        self.nl = len(ch)  # number of detection layers
+        self.reg_max = 16  # DFL channels (ch[0] // 16 to scale 4/8/12/16/20 for n/s/m/l/x)
+        self.no = nc + self.reg_max * 4  # number of outputs per anchor
+        self.stride = torch.zeros(self.nl)  # strides computed during build     
+        self.stem = nn.ModuleList(nn.Sequential(3DLEBlock(x, x, 1), 3DLEBlock(x, x, 1)) for x in ch)  
+        self.cv2 = nn.ModuleList(nn.Conv2d(x, 4 * self.reg_max, 1) for x in ch)
+        self.cv3 = nn.ModuleList(nn.Conv2d(x, self.nc, 1) for x in ch)
+        self.dfl = DFL(self.reg_max) if self.reg_max > 1 else nn.Identity()
+#-------------------------------------------------------------MLE_detect_head----------------------------
+
+#-------------------------------------------------------------3DLEBlock----------------------------
+class 3DAttention(torch.nn.Module):
+    def __init__(self, e_lambda=1e-4):
+        super(Attention, self).__init__()
+
+        self.activaton = nn.Sigmoid()
+        self.e_lambda = e_lambda
+
+    def __repr__(self):
+        s = self.__class__.__name__ + '('
+        s += ('lambda=%f)' % self.e_lambda)
+        return s
+
+    @staticmethod
+    def get_module_name():
+        return "3DAttention"
+
+    def forward(self, x):
+        b, c, h, w = x.size()
+
+        n = w * h - 1
+
+        x_minus_mu_square = (x - x.mean(dim=[2, 3], keepdim=True)).pow(2)
+        y = x_minus_mu_square / (4 * (x_minus_mu_square.sum(dim=[2, 3], keepdim=True) / n + self.e_lambda)) + 0.5
+
+        return x * self.activaton(y)
+
+class 3DLEBlock(nn.Module):
+
+    def __init__(self, c1, c2, k=1, s=1, g=1, act=True):
+
+        super().__init__()
+        c_ = c2 // 2  # hidden channels
+        self.cv1 = Conv(c1, c_, k, s, None, g, act=act)
+        self.attention = 3DAttention()
+        self.cv2 = Conv(c_, c_, 5, 1, None, c_, act=act)
+
+    def forward(self, x):
+        y = self.cv1(x)
+        y = self.attention(y)
+        return torch.cat((y, self.cv2(y)), 1)
+#-------------------------------------------------------------3DLEBlock-------------------------
